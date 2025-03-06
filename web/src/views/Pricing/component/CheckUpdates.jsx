156a127a4b2468eb6a31d5422d28f1eb308bd331
wood chen <wood@czl.net>
2025-03-06 22:28:46 +0800

    feat(CheckUpdates): 更新价格数据源URL

--- web/src/views/Pricing/component/CheckUpdates.jsx
+++ web/src/views/Pricing/component/CheckUpdates.jsx
@@ -22,7 +22,7 @@ import Label from 'ui-component/Label';
 
 export const CheckUpdates = ({ open, onCancel, onOk, row }) => {
   const { t } = useTranslation();
-  const [url, setUrl] = useState('https://raw.githubusercontent.com/MartialBE/one-api/prices/prices.json');
+  const [url, setUrl] = useState('https://aimodels-prices.q58.club/api/prices/rates');
   const [loading, setLoading] = useState(false);
   const [updateLoading, setUpdateLoading] = useState(false);
   const [newPricing, setNewPricing] = useState([]);
